da8b436b6649ef0b9d058a40cb31205d0879c5bc
Tamás Györkis <tamasgyorkis@gmail.com>
2022-12-11 22:19:02 +0100

    help me

--- nodejs/nodeapi.js
+++ nodejs/nodeapi.js
@@ -1,18 +1,13 @@
 let express = require('express');
 const bodyParser = require('body-parser');
-let puzzles = require('./puzzles.js');
+let drawings = require('./puzzles.js');
 let user = require('./users.js');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/drawings', (req, res) => {
-	res.set('Content-Type', 'application/json');
-	puzzles.getdrawings(req, res);
-});
-
-//LOGIN
+//Login
 
 app.post('/login', (req, res) => {
 	res.set('Content-Type', 'application/json');
@@ -25,6 +20,38 @@ app.post('/register', (req, res) => {
 	res.set('Content-Type', 'application/json');
 	user.register(req, res);
 });
+
+// Get all drawings
+app.get('/drawings', (req, res) => {
+	res.set('Content-Type', 'application/json');
+	drawings.getdrawings(req, res);
+});
+
+//Get single drawing
+app.get('/drawing', (req, res) => {
+	res.set('Content-type', 'application/json');
+	drawings.getSingleDrawing(req, res);
+});
+
+// Insert new drawing
+app.post('/drawing', (req, res) => {
+	res.set('Content-Type', 'application/json');
+	drawings.newdrawing(req, res);
+});
+
+// Update drawing
+
+app.put('/drawing', (req, res) => {
+	res.set('Content-Type', 'application/json');
+	drawings.UpdateDrawing(req, res);
+});
+
+// Delete drawing
+app.delete('/drawing', (req, res) => {
+	res.set('Content-Type', 'application/json');
+	drawings.deleteDrawing(req, res);
+});
+
 
 let server = app.listen(8080, () => {
 	let host = server.address().address
--- nodejs/puzzles.js
+++ nodejs/puzzles.js
@@ -22,11 +22,12 @@ exports.getdrawings = (req, res) => {
 				error: 1,
 				message: err
 			};
+			res.status(400);
 			res.send(json);
 			return;
 		}
 
-		let query = 'SELECT d.id, u.username, d.description, d.drawing_data FROM drawings d left join users u on u.id = d.user_id';
+		let query = 'SELECT d.id, u.username, d.title, d.drawing_data FROM drawings d left join users u on u.id = d.user_id';
 
 		dbConnection.query(query, (err, result) => {
 			if (err) {
@@ -34,6 +35,7 @@ exports.getdrawings = (req, res) => {
 					error: 1,
 					message: err
 				};
+				res.status(400);
 				res.send(json);
 				return;
 			}
@@ -51,3 +53,120 @@ exports.getdrawings = (req, res) => {
 
 	});
 }
+
+exports.getSingleDrawing = (req, res) => {
+	if (req.query?.drawing_id == undefined) {
+		let response = {
+			error: 1,
+			message: 'Missing parameters'
+		}
+		res.status(400);
+		res.send(response);
+		return;
+	}
+
+
+	let conn = GetConnection();
+
+	let query = "SELECT d.id, u.username, d.title, d.drawing_data FROM drawings d left join users u on u.id = d.user_id WHERE d.id = '" + req.query.drawing_id + "';";
+	conn.query(query, (error, result) => {
+		if (error) {
+			let response = {
+				error:1,
+				message: error
+			}
+			res.status(400);
+			res.send(response);
+			return;
+		}
+
+		if (result[0]?.id == undefined) {
+			let response = {
+				error:1,
+				message: 'Drawing not found!'
+			}
+			res.status(404);
+			res.send(response);
+			return;
+		}
+
+		let response = {
+			error: 0,
+			message: 'Drawing data:',
+			data: result[0]
+		}
+
+		res.send(response);
+
+	});
+
+};
+
+exports.newdrawing = (req, res) => {
+	if (req.body?.token == undefined || req.body?.title == undefined || req.body?.drawing_data == undefined) {
+		let response = {
+			error: 1,
+			message: 'Missing parameters!'
+		}
+		res.status(400);
+		res.send(response);
+		return;
+	}
+
+	if (req.body.drawing_data.length != 100 || isNaN(req.body.drawing_data)) {
+		let response = {
+			error:1,
+			message: 'Drawing data incorrect!',
+			length: req.body.drawing_data.length,
+		}
+		res.status(400);
+		res.send(response);
+		return;
+	}
+
+	let conn = GetConnection();
+	let query = "SELECT user_id FROM tokens WHERE token = '" + req.body.token + "' AND expire_date > now();"
+	conn.query(query, (error, response) => {
+		if (error) {
+			let response = {
+				error: 1,
+				message: error
+			}
+			res.send(response);
+			return;
+		}
+
+		let id = response[0]?.user_id;
+
+		if (!id) {
+			let response = {
+				error: 1,
+				message: 'Token invalid'
+			}
+			res.status(403);
+			res.send(response);
+			return;
+		}
+
+		let insertQuery = "INSERT INTO drawings(user_id, title, drawing_data) VALUES (" + id + ", '" + req.body.title + "', '" + req.body.drawing_data + "');";
+		conn.query(insertQuery, (error, response) => {
+			if (error) {
+				let response = {
+					error: 1,
+					message: error
+				}
+				res.send(response);
+				return;
+			}
+
+			let response2 = {
+				error: 0,
+				message: 'Drawing sucessfully uploaded!'
+			};
+			res.status(201);
+			res.send(response2);
+
+		});
+
+	});
+};
